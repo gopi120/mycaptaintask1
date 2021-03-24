d51f55ef43092bf98a1bbd21e0694e10375634c2
gopi120 <80970317+gopi120@users.noreply.github.com>
2021-03-24 13:46:46 +0530

    Task2.c
    Print following instructions

---
+++ Task2.c
@@ -0,0 +1,57 @@
+#include"declaration.h"
+
+#include"welcome.h" 3 #include"menu.h"
+
+#include "newscoresheet.h" #include "Filing.h"
+
+#include"final.h" 7 int main()
+
+system("color f1");
+
+int a, keys char b, ch;
+
+FILE "fnew;
+
+char lastinput; while (TRUE)
+
+initializeconsolehandles();
+
+welcome(); system("cls");
+
+menu(); locate(32,9);
+
+ch=getch();
+
+switch(ch)
+
+char lastinput; while (TRUE)
+
+{
+
+initializeconsolehandles();
+
+welcome(); system("cls");
+
+menu();
+
+locate(32,9);
+
+ch=getch();
+switch(ch)
+
+case '1':
+
+fileopen(ch);
+
+case '2':
+
+fileopen(ch);
+
+getch();
+
+case '3':
+
+system ("cls");
+
+exit();
+
